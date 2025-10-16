a466ee2c5f57b60a9145c3d2915d76994969d9c0
Vladyslav Kyrpa <vladkrp2003@gmail.com>
2025-10-16 22:13:52 +0300

    add home page

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,11 +1,33 @@
-export default function HomePage() {
-    // todo: 
-    // search bar for decks (public)
-    // to my decks button
-    // to saved decks button 
-    // create new deck button
-    // statistics
-    return <div>
+import { useNavigate } from "react-router-dom";
+import Button from "../shared/Buttons";
+import SearchBar from "../shared/SearchBar";
 
+export default function HomePage() {
+    const navigate = useNavigate();
+
+    const onSearch = (value) => {
+        navigate(`/search?value=${value}`);
+    }
+
+    const toUserDecks = () => {
+        navigate("/my-decks");
+    }
+
+    const toSavedDecks = () => {
+        navigate("/saved-decks");
+    }
+
+    const toCreateDeck = () => {
+        navigate("/create-deck");
+    }
+
+    // todo: 
+    // add user statistics block
+
+    return <div className="flex flex-col gap-2">
+        <SearchBar placeholder="Type deck name..." onSearch={onSearch} />
+        <Button onClick={toUserDecks}>My Decks</Button>
+        <Button onClick={toSavedDecks}>Saved Decks</Button>
+        <Button onClick={toCreateDeck}>+ Create Deck</Button>
     </div>
 }
