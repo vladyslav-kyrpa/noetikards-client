4422387e596153f747454946b29219e64032ea4f
Vladyslav Kyrpa <vladkrp2003@gmail.com>
2025-10-16 21:01:49 +0300

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Outlet, Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
+import Layout from "./components/shared/Layout";
+
+const LayoutWrapper = () => {
+  return <Layout>
+    <Outlet />
+  </Layout>
+}
 
 export default function App() {
   return <HashRouter>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route element={<LayoutWrapper />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
     </Routes>
   </HashRouter>
 }
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,3 +1,11 @@
 export default function HomePage() {
-    return <div>Hello World</div>
+    // todo: 
+    // search bar for decks (public)
+    // to my decks button
+    // to saved decks button 
+    // create new deck button
+    // statistics
+    return <div>
+
+    </div>
 }
